f2a2775f4e223f605fac7e31a28f3c5202c60aed
imvee <vind.veeru@gmail.com>
2021-07-24 20:58:34 +0530

    :zap: Naive Bayes

--- spam email/spam_mail.py
+++ spam email/spam_mail.py
@@ -12,9 +12,16 @@ def accuracy(y_true, y_pred):
     accuracy = np.sum(y_true == y_pred) / len(y_true)
     return accuracy
 
-X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
-    
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+# X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+
+train_df=pd.read_csv('train_data.csv')
+test_df=pd.read_csv('test_features.csv')
+
+train_df=train_df.drop('Id',axis=1)
+test_df=test_df.drop('Id',axis=1)
+
+X_train=
 
 nb = NaiveBayes()
 nb.fit(X_train, y_train)
